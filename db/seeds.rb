bfaaa0dccbe3b3956562471e83d4beece4d23fc1
Ace Macariola <falconsys32@gmail.com>
2021-05-12 10:41:36 -0400

    added seed data for the library

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.delete_all
+Book.delete_all
+
+100.times do
+    author = Author.create! name: Faker::Book.author, biography: Faker::Lorem.paragraph(sentence_count: 3), genres: Faker::Book.genre
+    puts "Created new author: #{author.name}"
+
+    10.times do
+       book = Book.create!(
+	  title: Faker::Book.title,
+          description: Faker::Lorem.paragraph(sentence_count: 3),
+          ratings: rand(1..5),
+          author_id: author.id
+       )
+       puts "Created Book #{book.title} for author #{book.author_id}"
+    end
+
+end
